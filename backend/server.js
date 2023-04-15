4e3d6aedb645c697968ad7d44bcacb85e7571482
Muthu Kumar K <81695445+MuthuKumarKRIT@users.noreply.github.com>
2023-04-15 11:17:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true ,useUnifiedTopology:true,useCreateIndex:true}
 );
 const connection = mongoose.connection;
